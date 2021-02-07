45b07a3f4bb0f7207aa062784c4e9737e3c4bfc7
Harry <harrybest800@gmail.com>
2021-02-07 14:00:34 +0000

    Spaced the days buttons

--- src/DayButtons.js
+++ src/DayButtons.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Row } from 'antd';
 
 function DayButtons({days}) {
     return (
     <>
+        <Row justify='space-around'>
         <Button shape="circle" className="dayButton" type={days.mon ? 'primary' : 'dashed'}>M</Button>
         <Button shape="circle" className="dayButton" type={days.tue ? 'primary' : 'dashed'}>T</Button>
         <Button shape="circle" className="dayButton" type={days.wed ? 'primary' : 'dashed'}>W</Button>
@@ -11,6 +12,7 @@ function DayButtons({days}) {
         <Button shape="circle" className="dayButton" type={days.fri ? 'primary' : 'dashed'}>F</Button>
         <Button shape="circle" className="dayButton" type={days.sat ? 'primary' : 'dashed'}>S</Button>
         <Button shape="circle" className="dayButton" type={days.sun ? 'primary' : 'dashed'}>S</Button>
+        </Row>
     </>
     )
 }
